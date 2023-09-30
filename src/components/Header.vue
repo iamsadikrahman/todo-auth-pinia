e4f934a3239e4ea8d9dcf82342887b7ff9de9ff2
iamsadikrahman <iamsadikrahman@outlook.com>
2023-09-30 22:00:54 +0600

    login and register page added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,11 +4,11 @@
 
 <template>
     <nav class="bg-purple-500 flex justify-between  flex-wrap px-10 py-5">
-        <h1 class="text-2xl font-bold"><i class="fa-solid fa-clipboard-list mr-2"></i>Todo List</h1>
+        <router-link to="/"><h1 class="text-2xl font-bold"><i class="fa-solid fa-clipboard-list mr-2"></i>Todo List</h1></router-link>
         <ul class="flex space-x-10 text-lg font-semibold">
-        <li>Home</li>
-        <li>Login</li>
-        <li>Registration</li>
+        <router-link to="/"><li>Home</li></router-link>
+        <router-link to="/login"><li>Login</li></router-link>
+        <router-link to="/register"><li>Register</li></router-link>
         </ul>
     </nav>
 </template>
--- src/router/router.js
+++ src/router/router.js
@@ -14,7 +14,7 @@ const routes = [
         component: Login,
     },
     {
-        path: '/registration',
+        path: '/register',
         component: Registration
     }
 ]
